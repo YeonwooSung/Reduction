e2475d811f199bac95e92283969877b108e2da47
YeonwooSung <neos960518@gmail.com>
2019-04-10 04:40:50 +0900

    initial commit

---
+++ src/SATTo3SAT.java
@@ -0,0 +1,4 @@
+
+public class SATTo3SAT {
+
+}
---
+++ src/StraightforwardReduction.java
@@ -0,0 +1,13 @@
+
+/**
+ * Main class for task 1.
+ *
+ * @author YeonwooSung
+ */
+public class StraightforwardReduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
