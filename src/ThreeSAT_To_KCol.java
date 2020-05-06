cf89a42317edbcf63d2af32caa12627988c6ea98
YeonwooSung <neos960518@gmail.com>
2020-05-06 21:44:08 +0900

    Remove unused imports

--- src/ThreeSAT_To_KCol.java
+++ src/ThreeSAT_To_KCol.java
@@ -1,14 +1,9 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Writer;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class ThreeSAT_To_KCol {
